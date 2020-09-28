cbec4956433b86b3987f88adb12f0aa85936f721
skhademcis <43582925+skhademcis@users.noreply.github.com>
2020-09-28 10:32:23 -0700

    Add files via upload

--- assets/cli-collector/encore.sh
+++ assets/cli-collector/encore.sh
@@ -58,8 +58,32 @@ setup() {
 }
 
 init() {
-    pythonVersion=`$pybin -V 2>&1 | grep "Python 3*"`
-#    echo "Python Version " + sys.version()
+    pythonVersion=`$pybin -V 2>&1 | grep "Python"`
+    if [ "$pythonVersion" != "" ]
+    then
+        : echo $pythonVerison
+    fi
+
+    # This only seems necessary on Linux outside of Splunk
+    pyMaxUnicodeOk=`$pybin -c 'import sys; print(0x10000 < sys.maxunicode)'`
+    if [ "$pyMaxUnicodeOk" = "False" ]
+    then
+        pyPath=`which python`
+
+        echo 'Incompatible build of python'
+        echo
+        echo 'eNcore requires the standard version of python built with PyUnicodeUCS4. You are'
+        echo 'seeing this error message because the currently executing version of python'
+        echo "($pyPath) is built with PyUnicodeUCS2."
+        echo
+        echo 'This is a known issue in certain cases when running as the splunk user (more here:'
+        echo 'https://answers.splunk.com/answers/327336/why-am-i-getting-importerror-undefined-symbol-pyun.html)'
+        echo
+        echo 'The solution is to create a new user with the standard python environment and run'
+        echo 'as that user.'
+        echo
+        exit $EXIT_CODE_ERROR
+    fi
 
     if [ ! -e "$configFilepath" ]
     then

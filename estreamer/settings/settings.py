23f24e04d321534da344d3cda9aab3a6b0981488
skhademcis <43582925+skhademcis@users.noreply.github.com>
2022-09-01 13:20:04 -0400

    Update settings.py

--- estreamer/settings/settings.py
+++ estreamer/settings/settings.py
@@ -113,6 +113,7 @@ class Settings( object ):
         self.writeCore = handlerRecords['core']
         self.writeMetadata = handlerRecords['metadata']
         self.writeConnections = True
+        self.writeVPN = True
         if 'connections' in handlerRecords:
             self.writeConnections = handlerRecords['connections']
 
@@ -327,3 +328,4 @@ class Settings( object ):
 
     def toBase64( self ):
         return base64dump( self.store )
+    
